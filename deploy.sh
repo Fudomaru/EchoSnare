8b606c7427558de6eea66df8b799d9f9bce0dac8
Marcel <herrbuschmarcel@gmail.com>
2026-02-15 00:23:28 +0100

    Adding basic NTP Update over local WLAN

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,4 @@ echo "[*] Deploying all src/ files to ESP32..."
 
 mpremote connect "$PORT" fs cp -r src/* :
 
-echo "[*] Resetting device..."
-mpremote connect "$PORT" reset
-
 echo "[✓] Deployment complete."
